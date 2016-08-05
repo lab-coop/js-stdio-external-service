7a75d8feb8008e774283c37510817c01efe3a086
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2016-08-05 19:49:28 +0200

    refactor(console-test): separate message transform and stream selection

--- lib/console.spec.ts
+++ lib/console.spec.ts
@@ -3,9 +3,17 @@
 import test from 'ava'
 import console from './console'
 
+test('it should add a new line after the message', t => {
+  const {message} = console.log({level: 'debug', message: 'apple'})
+  t.deepEqual(message, 'apple\n')
+})
+
 test('debug should write to stdout', t => {
-  t.deepEqual(console.log({level: 'debug', message: 'apple'}), {
-    stream: 'stdout',
-    message: 'apple\n'
-  })
+  const {stream} = console.log({level: 'debug', message: 'apple'})
+  t.deepEqual(stream, 'stdout')
+})
+
+test('info should write to stdout', t => {
+  const {stream} = console.log({level: 'info', message: 'apple'})
+  t.deepEqual(stream, 'stdout')
 })
